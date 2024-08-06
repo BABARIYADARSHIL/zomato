a0cbf20083c2747083e5c8976502413af08d3c85
BABARIYADARSHIL <darshilbabariya444.com>
2024-08-06 14:17:19 +0530

    Some-Change-python

--- src/api/restaurant/api/index.tsx
+++ src/api/restaurant/api/index.tsx
@@ -1,3 +1,6 @@
+export const LocalApi = 'http://127.0.0.1:5500/'
 export const restaurantdatasearch = 'search'
-export const data = `http://127.0.0.1:5500/${restaurantdatasearch}`;
-export const restaurantdata = 'http://127.0.0.1:5500/restaurant'
+export const restaurant = 'restaurant'
+
+export const data = `${LocalApi}${restaurantdatasearch}`;
+export const restaurantdata = `${LocalApi}${restaurant}`;
