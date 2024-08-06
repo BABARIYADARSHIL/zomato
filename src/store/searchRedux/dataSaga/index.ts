56d5376d1a6ded562559aa907b0f17c99aef3c9a
BABARIYADARSHIL <darshilbabariya444.com>
2024-08-06 14:20:58 +0530

    Api-Name-Change

--- src/store/searchRedux/dataSaga/index.ts
+++ src/store/searchRedux/dataSaga/index.ts
@@ -1,12 +1,12 @@
 import { call, put, takeEvery } from "redux-saga/effects";
-import { RestaurantData } from "../../../api/restaurant/RestaurantData";
+import { RestaurantSearch } from "../../../api/restaurant/RestaurantSearch";
 import { DATA_FETCHING } from "../type/index";
 import { datafetching, datafetchingFail, datafetchingSuccess } from "../action";
 import { ResponseData } from "../../../types/SearchType";
 
 export function* restaurantData(action: ReturnType<typeof datafetching>) {
   try {
-    const response: ResponseData = yield call(RestaurantData, {
+    const response: ResponseData = yield call(RestaurantSearch, {
       search: action.payload.searchTerm,
     });
     yield put(datafetchingSuccess(response.data));
