2368ebb2ef6bd05108dbc672e4a511f9a2f0b34e
BABARIYADARSHIL <darshilbabariya444.com>
2024-08-07 15:13:35 +0530

    some-Chnage-SearchSuggestion

--- src/component/searchSuggestions/Home.tsx
+++ src/component/searchSuggestions/Home.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import SearchSuggestionsComponent from './index';
-import { SearchSuggestionsComponentProps, Suggestion } from '../../types/SearchType';
+import { SearchSuggestionsComponentProps } from '../../types/SearchType';
 
 const HomeSearchSuggestionsComponent: React.FC<SearchSuggestionsComponentProps> = (props) => {
-    const getNavLinkPath = (suggestion: Suggestion) => `/Restaurant`;
+    const getNavLinkPath = () => `/Restaurant`;
 
     return (
         <SearchSuggestionsComponent {...props} getNavLinkPath={getNavLinkPath} />
